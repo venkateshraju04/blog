0818e5c7dcf1db739f36841a2d7e88fc156cb576
venkatesh <venkatesh1754673niz@gmail.com>
2025-09-10 17:59:07 +0530

    fix: removed different project types

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,37 +15,20 @@ export default async function Projects() {
       }
     })
   )
-  const workProjects = PROJECTS.filter(({ type }) => type === 'work')
   const sideProjects = PROJECTS.filter(({ type }) => type === 'self')
 
   return (
     <Container className="pt-4 lg:pt-12">
       <PageHeader
         title="Projects"
-        description="Collections of my open-source side projects, along with some cool things I’ve built with colleagues at work. It’s a mix of passion projects and practical tools—some just for fun, others to solve real-world problems."
+        description="Some of the cool things I've built over the years."
         className="border-b border-gray-200 dark:border-gray-700"
       />
-      <div className="py-5 md:py-10">
-        <h3 className="mb-6 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-3xl">
-          Work
-        </h3>
-        <div className="space-y-16">
-          {workProjects.map((pro, idx) => (
-            <ProjectCard key={pro.title} project={pro} reversed={idx % 2 === 1} />
-          ))}
-        </div>
-      </div>
+
       <div className="mt-6 border-t border-gray-200 py-5 dark:border-gray-700 md:mt-10 md:py-10">
-        <h3 className="mb-6 text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:mb-8 md:text-3xl">
-          Side projects
-        </h3>
         <div className="space-y-16">
           {sideProjects.map((pro, idx) => (
-            <ProjectCard
-              key={pro.title}
-              project={pro}
-              reversed={workProjects.length % 2 === 1 && idx % 2 === 0}
-            />
+            <ProjectCard key={pro.title} project={pro} />
           ))}
         </div>
       </div>
